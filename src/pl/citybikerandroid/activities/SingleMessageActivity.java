167ec93c3c89a2b43e7fb4e964587a5b58f13906
siemionides <siemionides@gmail.com>
2013-06-23 17:40:27 +0200

    added library for QR paring. to main actionbar added QR-serach icon. added using QRs functionality (even downloading BarCode Scanner when tehres no app installed)

--- src/pl/citybikerandroid/activities/SingleMessageActivity.java
+++ src/pl/citybikerandroid/activities/SingleMessageActivity.java
@@ -13,6 +13,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.ViewDebug.CapturedViewProperty;
 import android.widget.TextView;
 import android.widget.Toast;
 
